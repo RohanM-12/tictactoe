c862f3b0cfbdbbe5fe4d266adf110178ac52f567
Rohan Mali <rohanmali.2002@gmail.com>
2023-01-22 21:58:59 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to React Vite Micro App!</h1>
+      <p>chalo tictactoe banaye</p>
+    </>
+  );
+};
+
+export default App;
